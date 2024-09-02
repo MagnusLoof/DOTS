6fe2580a397cbdd5b8bc066cd0040c32cc8f5dd2
MagnusLoof <144233784+MagnusLoof@users.noreply.github.com>
2024-09-02 15:08:15 +0200

    Initial commit

---
+++ Assets/Scripts/SpawnerSystem.cs
@@ -0,0 +1,23 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public partial struct SpawnerSystem : ISystem
+{
+    public void OnCreate(ref SystemState state) { }
+    public void OnDestroy(ref SystemState state) { }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (RefRW<Spawner> spawner in SystemAPI.Query<RefRW<Spawner>>())
+        {
+            if (spawner.ValueRO.NextSpawnTime < SystemAPI.Time.ElapsedTime)
+            {
+                Entity newEntity = state.EntityManager.Instantiate(spawner.ValueRO.PreFab);
+                float3 position = new float3(spawner.ValueRO.SpawnPosition, 0);
+                state.EntityManager.SetComponentData(newEntity, LocalTransform.FromPosition(position));
+                spawner.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + spawner.ValueRO.SpawnRate;
+            }
+        }
+    }
+}
